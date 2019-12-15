4c0e8f741454c1b7b5457e57da2560c6712ebd55
Maika Kailani <queenmab051@gmail.com>
2019-12-15 22:24:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-class Artist
+class Song
   
   extend Memorable::ClassMethods
   extend Findable
@@ -8,41 +8,15 @@ class Artist
   include Paramble
   
   attr_accessor :name
-  attr_reader :songs
-
-  @@artists = []
-
-  def initialize
-    @@artists << self
-    super
-    @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
+  attr_reader :artist
+  
+  @@songs = []
+  
   def self.all
-    @@artists
+    @@songs
   end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
-  end
-
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
+  
+  def artist=(artist)
+    @artist = artist
   end
 end
